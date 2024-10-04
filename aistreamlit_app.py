8330646d7c842d9a875d54007ea2028a6ff939c0
FirePBlast <lucasmismacque@gmail.com>
2024-10-04 10:36:38 +0200

    Update aistreamlit_app.py

--- aistreamlit_app.py
+++ aistreamlit_app.py
@@ -8,10 +8,43 @@ if 'url' not in st.session_state:
 
 # Fonctions pour chaque page
 def page1():
-    st.title("Page 1")
+    
+    # Initialize chat history
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+    
+    # Display chat messages from history on app rerun
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+    
+    # React to user input
+    if prompt := st.chat_input("What is up?"):
+        # Display user message in chat message container
+        st.chat_message("user").markdown(prompt)
+        # Add user message to chat history
+        st.session_state.messages.append({"role": "user", "content": prompt})
+    
+        # url = 'https://42b8-34-168-18-253.ngrok-free.app/question'
+        
+        data={
+            "prompt" : prompt
+        }
+        response = requests.get(st.session_state['url'], params=data)
+        
+        if response.status_code == 200:
+            response_content = response.json()  # Extrayez le contenu JSON
+        else:
+            response_content = f"Erreur : {response.status_code}"
+             
+        # Display assistant response in chat message container
+        with st.chat_message("assistant"):
+            st.markdown(response_content )
+        # Add assistant response to chat history
+        st.session_state.messages.append({"role": "assistant", "content": response_content })
 
 def page2():
-      st.title("Formulaire de la page 2")
+      st.title("Lecture de Table")
       email = st.text_input("Email")
       country = st.selectbox("Pays", ["France", "USA", "Canada"])
     
@@ -19,9 +52,9 @@ def page3():
     st.title("Page 3")
 
 pages = {
-    "Page 1": page1,  
-    "Page 2": page2,
-    "Page 3": page3,
+    "Chat Bot": page1,  
+    "Lecture de Table": page2,
+    "Monitoring": page3,
 }
 
 with st.sidebar:
@@ -37,39 +70,6 @@ with st.sidebar:
 if selected_page:
     pages[selected_page]()
 
-# Initialize chat history
-if "messages" not in st.session_state:
-    st.session_state.messages = []
-
-# Display chat messages from history on app rerun
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
-
-# React to user input
-if prompt := st.chat_input("What is up?"):
-    # Display user message in chat message container
-    st.chat_message("user").markdown(prompt)
-    # Add user message to chat history
-    st.session_state.messages.append({"role": "user", "content": prompt})
-
-    # url = 'https://42b8-34-168-18-253.ngrok-free.app/question'
-    
-    data={
-        "prompt" : prompt
-    }
-    response = requests.get(st.session_state['url'], params=data)
-    
-    if response.status_code == 200:
-        response_content = response.json()  # Extrayez le contenu JSON
-    else:
-        response_content = f"Erreur : {response.status_code}"
-         
-    # Display assistant response in chat message container
-    with st.chat_message("assistant"):
-        st.markdown(response_content )
-    # Add assistant response to chat history
-    st.session_state.messages.append({"role": "assistant", "content": response_content })
 
         
 
